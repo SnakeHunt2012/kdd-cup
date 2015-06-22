37deb34a230ff0985d4b5500d9ead3375a6f119a
SnakeHunt2012 <SnakeHunt2012@gmail.com>
2015-06-22 13:37:37 +0800

    Modify script develop.py, tunning parameters with multiprocessing.

--- kdd-cup-2015/scripts/develop.py
+++ kdd-cup-2015/scripts/develop.py
@@ -1,13 +1,21 @@
 #!/bin/env python2.7
 
+import os
+import time
+import json
+import thread
 import logging
 
 import numpy as np
 import pandas as pd
 
-from sklearn.cross_validation import StratifiedShuffleSplit
-from sklearn.ensemble import RandomForestClassifier
+from collections import OrderedDict
+from multiprocessing import Process
+
 from sklearn.metrics import roc_auc_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.grid_search import ParameterGrid
+from sklearn.cross_validation import StratifiedShuffleSplit
 
 def load_data(path):
 
@@ -81,7 +89,7 @@ def load_train_data():
 
     return X_train, X_validate, y_train, y_validate
 
-def make_submission(classifier, path="../data/submission.csv"):
+def make_submission(classifier, parma_dict, filename="submission"):
 
     logger = logging.getLogger("make_submission")
     logger.setLevel(logging.INFO)
@@ -94,18 +102,21 @@ def make_submission(classifier, path="../data/submission.csv"):
 
     logger.addHandler(ch)
 
-    logger.info("predict start ...")
     id_test, X_test = load_test_data()
+
+    logger.info("predict start ...")
     y_test_pred = classifier.predict_proba(X_test)
     logger.info("y_test_pred.shape = %r", y_test_pred.shape)
     logger.info("predict end.")
 
-    logger.info("dump submission.csv start ...")
+    path = "../data/%s.csv" % filename
     submission = pd.DataFrame({"enrollment_id": id_test, "dropout": y_test_pred[:, -1]})
+    
+    logger.info("dump %s start ...", path)
     submission.to_csv(path, columns = ["enrollment_id", "dropout"], header=False, index=False)
-    logger.info("dump submission.csv end.")
-
-def train_validate_test():
+    logger.info("dump %s end.", path)
+    
+def train_validate_test(param_dict):
 
     logger = logging.getLogger("develop")
     logger.setLevel(logging.INFO)
@@ -125,21 +136,21 @@ def train_validate_test():
 
     logger.info("train start ... ")
     classifier = RandomForestClassifier(
-        n_estimators = 200,
-        criterion = "gini",
-        max_features = None,
-        max_depth = None,
-        min_samples_split = 2,
-        min_samples_leaf = 15,
-        min_weight_fraction_leaf = 0.0,
-        max_leaf_nodes = None,
-        bootstrap = True,
-        oob_score = False,
-        n_jobs = -1,
-        random_state = None,
-        verbose = 1,
-        warm_start = False,
-        class_weight = None,
+        n_estimators = param_dict["n_estimators"],
+        criterion = param_dict["criterion"],
+        max_features = param_dict["max_features"],
+        max_depth = param_dict["max_depth"],
+        min_samples_split = param_dict["min_samples_split"],
+        min_samples_leaf = param_dict["min_samples_leaf"],
+        min_weight_fraction_leaf = param_dict["min_weight_fraction_leaf"],
+        max_leaf_nodes = param_dict["max_leaf_nodes"],
+        bootstrap = param_dict["bootstrap"],
+        oob_score = param_dict["oob_score"],
+        n_jobs = param_dict["n_jobs"],
+        random_state = param_dict["random_state"],
+        verbose = param_dict["verbose"],
+        warm_start = param_dict["warm_start"],
+        class_weight = param_dict["class_weight"]
     )
     classifier.fit(X_train, y_train)
     logger.info("train end.")
@@ -147,15 +158,80 @@ def train_validate_test():
     logger.info("predict start ...")
     y_train_pred = classifier.predict_proba(X_train)
     y_validate_pred = classifier.predict_proba(X_validate)
-    logger.info("training set auc: %r", roc_auc_score(y_train, y_train_pred[:, -1]))
-    logger.info("validateion set auc: %r", roc_auc_score(y_validate, y_validate_pred[:, -1]))
+    auc_train = roc_auc_score(y_train, y_train_pred[:, -1])
+    auc_validate = roc_auc_score(y_validate, y_validate_pred[:, -1])
+    logger.info("training set auc: %r", auc_train)
+    logger.info("validateion set auc: %r", auc_validate)
     logger.info("predict end.")
 
-    make_submission(classifier)
+    timestamp = time.strftime("[%Y-%m-%d]_[%H-%M-%S]", time.localtime(time.time()))
+    filename = "submission_rf_%s_[%d]_[%d]" % (timestamp, os.getpid(), thread.get_ident())
+    path = "../data/%s.json" % filename
+    
+    meta = OrderedDict()
+    meta["model"] = "RandomForest"
+    meta["param_dict"] = param_dict
+    meta["auc_train"] = auc_train
+    meta["auc_validate"] = auc_validate
+    
+    logger.info("dump %s start ...", path)
+    with open(path, 'w') as fp:
+        json.dump(meta, fp, indent=4)
+    logger.info("dump %s end.", path)
+
+    make_submission(classifier, param_dict, filename=filename)
 
 def develop():
 
-    train_validate_test()
+    param_grid = [
+        {
+            "n_estimators"             : [200],
+            "criterion"                : ["gini"],
+            "max_features"             : [None],
+            "max_depth"                : [None],
+            "min_samples_split"        : [3, 6, 9, 12, 15],
+            "min_samples_leaf"         : [3, 6, 9, 12, 15],
+            "min_weight_fraction_leaf" : [0.0],
+            "max_leaf_nodes"           : [None],
+            "bootstrap"                : [True],
+            "oob_score"                : [False],
+            "n_jobs"                   : [-1],
+            "random_state"             : [None],
+            "verbose"                  : [1],
+            "warm_start"               : [False],
+            "class_weight"             : [None],
+        },
+        {
+            "n_estimators"             : [200],
+            "criterion"                : ["entropy"],
+            "max_features"             : [None],
+            "max_depth"                : [None],
+            "min_samples_split"        : [10, 15],
+            "min_samples_leaf"         : [2, 5, 10],
+            "min_weight_fraction_leaf" : [0.0],
+            "max_leaf_nodes"           : [None],
+            "bootstrap"                : [True],
+            "oob_score"                : [False],
+            "n_jobs"                   : [-1],
+            "random_state"             : [None],
+            "verbose"                  : [1],
+            "warm_start"               : [False],
+            "class_weight"             : [None],
+        }
+    ]
+    param_list = list(ParameterGrid(param_grid))
+    
+    process_list = []
+    for param_dict in param_list:
+        process = Process(target=train_validate_test,
+                          args=(param_dict, ))
+        process_list.append(process)
+
+    for process in process_list:
+        process.start()
+
+    for process in process_list:
+        process.join()
 
 def main():
 
