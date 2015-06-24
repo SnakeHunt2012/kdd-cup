e3fddd9e52a9d13c4c4b522a9d67e60b12b36545
SnakeHunt2012 <SnakeHunt2012@gmail.com>
2015-06-24 21:59:29 +0800

    Add function summary_svm() to script summary.py, extract develop results from ../data/submission_svm_*.json.

--- kdd-cup-2015/scripts/summary.py
+++ kdd-cup-2015/scripts/summary.py
@@ -4,9 +4,36 @@
 import os
 import json
 
-def summary_gb():
+def summary_svm(cmd="ls ../data/submission_svm_*.json"):
 
-    with os.popen("ls ../data/submission_gb_*.json") as fp:
+    with os.popen(cmd) as fp:
+        file_list = fp.read().split()
+
+    json_list = []
+    for json_file in file_list:
+        with open(json_file, 'r') as fp:
+            json_list.append((json_file, json.loads(fp.read())))
+    json_list.sort(key=lambda (json_file, json_dict):
+                   (json_dict["param_dict"]["scaler"],
+                    json_dict["param_dict"]["kernel"],
+                    json_dict["param_dict"]["class_weight"]))
+
+    output_format = "%f\t%f\t%s\t%s\t%s\t%s"
+    print ""
+    for json_file, json_dict in json_list:
+        print output_format % \
+            (
+                json_dict["auc_validate"],
+                json_dict["auc_train"] - json_dict["auc_validate"],
+                json_dict["param_dict"]["scaler"],
+                json_dict["param_dict"]["kernel"],
+                json_dict["param_dict"]["class_weight"],
+                json_file
+            )
+
+def summary_gb(cmd="ls ../data/submission_gb_*.json"):
+
+    with os.popen(cmd) as fp:
         file_list = fp.read().split()
 
     json_list = []
@@ -31,9 +58,9 @@ def summary_gb():
                 json_file
             )
 
-def summary_rf():
+def summary_rf(cmd="ls ../data/submission_rf_*.json"):
 
-    with os.popen("ls ../data/submission_rf_*.json") as fp:
+    with os.popen(cmd) as fp:
         file_list = fp.read().split()
 
     json_list = []
@@ -61,7 +88,8 @@ def summary_rf():
 def main():
 
     #summary_rf()
-    summary_gb()
+    #summary_gb("ls ../data/submission_gb_*.json")
+    summary_svm("ls ../data/submission_svm_*.json")
 
 if __name__ == "__main__":
 
